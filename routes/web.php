5f3e1d2062454eec7d2c27ad0fd617bf0514fac7
chaima-syc <chaimaelomrani6@gmail.com>
2025-02-24 20:30:07 +0000

    ameliorated the style of signup and login pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/login', [AuthController::class, 'login']);
+Route::get('register',[AuthController::class, 'register']);
